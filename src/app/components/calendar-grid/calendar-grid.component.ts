4c877272d638d108e5e72c28aabe98602b03085a
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-08-09 17:59:20 +0200

    Bug found Event created on other months other than current were not shown  FIXED
    Show Events created by the user who created
    Refactored

--- src/app/components/calendar-grid/calendar-grid.component.ts
+++ src/app/components/calendar-grid/calendar-grid.component.ts
@@ -16,35 +16,19 @@ export class CalendarGridComponent implements OnInit {
   @ViewChild('calendar') calendarRef:FullCalendarComponent
   public start;
   public end;
-  public flag=false;
   public height = 917;
-  public listEvent = [];
   public userLoggedIn: UserModel;
   public userId;
-  public openEv: boolean = false
   calendarOptions: CalendarOptions;
 
-  onEvent(start,end){
-    this.calendarService
-      .getEvents({start: start, end: end})
-      .pipe(
-        map((event) => event.filter(ev => ev.status === 'public'))
-      )
-      .subscribe((events: any) =>
-        this.calendarOptions = {...this.calendarOptions, events}
-      );
-
-  }
 
   ngOnInit() {
     this.userLoggedIn = JSON.parse(localStorage.getItem('event:user'));
     this.userId = this.userLoggedIn.id;
-    // console.log(this.authService.loggedUser);
     this.authService.logoutSubject
       .pipe(
         tap(() => {
           this.onEvent(this.start,this.end)
-          // console.log(this.calendarOptions)
         })
       ).subscribe()
 
@@ -59,10 +43,6 @@ export class CalendarGridComponent implements OnInit {
   ) {
     this.declarecalendar()
   }
-
-  // ngAfterViewChecked() {
-  //   this.declarecalendar()
-  // }
 
 
   declarecalendar() {
@@ -101,9 +81,7 @@ export class CalendarGridComponent implements OnInit {
         list: "list"
       },
 
-
-      // height: this.theHeight,
-      height: 917,
+       height: 917,
       contentHeight: 800,
       aspectRatio: 4,
       expandRows: true,
@@ -114,11 +92,9 @@ export class CalendarGridComponent implements OnInit {
       dateClick: (data) => {
         this.calendarService.selectedDate = data.date.toISOString()
         if (this.authService.isAuthenticated) this.router.navigate(['new'], {relativeTo: this.activatedRoute})
-        // this.openEv = true
       },
       eventClick: (data) => {
         this.router.navigate([data.event.id], {relativeTo: this.activatedRoute})
-        // this.openEv = true
       },
 
       //Called after the calendar’s date range has been initially set or changed in some way and the DOM has been updated.
@@ -138,22 +114,28 @@ export class CalendarGridComponent implements OnInit {
         if (this.authService.loggedUser) {
           this.calendarService
             .getEvents({start: s, end: e})
-            .subscribe((events: EventModel[]) => {
-              events.forEach((value, index, array) => {
-                if (value.user.id === this.userLoggedIn.id) {
-                  // console.log(value);
-                  this.listEvent.push(value)
-                  this.calendarOptions.events = this.listEvent;
-                  console.log(this.calendarOptions.events);
-                }
-              })
-            });
+            .pipe(
+              map((event) => event.filter(ev => ev.user.id === this.userLoggedIn.id))
+            )
+            .subscribe((events: any) =>
+              this.calendarOptions = {...this.calendarOptions, events}
+            );
+
         } else {
         this.onEvent(s,e)
         }
       }
     };
-
+  }
+  onEvent(start,end){
+    this.calendarService
+      .getEvents({start: start, end: end})
+      .pipe(
+        map((event) => event.filter(ev => ev.status === 'public'))
+      )
+      .subscribe((events: any) =>
+        this.calendarOptions = {...this.calendarOptions, events}
+      );
   }
 
 
