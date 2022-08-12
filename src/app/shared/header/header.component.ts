7b449a3880b082e1fa5fd75c41e12674da8f6252
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-08-12 16:03:17 +0200

    Set Img url changed logic

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,62 +1,60 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../Services/auth.service";
 import {UserModel} from "../../Model/user.model";
-import {map} from "rxjs";
-import {CalendarService} from "../../Services/calendar.service";
-import {CalendarGridComponent} from "../../components/calendar-grid/calendar-grid.component";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss',]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
   user: UserModel
-  imgUrl: string
+  name: string;
+
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
   ) {
-  }
 
-  name: string;
+  }
 
   ngOnInit(): void {
     if (this.authService.loggedUser) {
       this.name = this.authService.loggedUser.firstName
     }
     this.user = this.authService.loggedUser
-    this.setImg()
   }
+
 
   onSignOut(): void {
     this.authService.signOut();
     this.user = null
   }
 
-  Login(){
+  Login() {
     this.router.navigateByUrl('/auth/signin')
   }
-  SignUp(){
+
+  SignUp() {
     this.router.navigateByUrl('/auth/signup')
   }
 
-  newEvent(){
+  newEvent() {
     this.router.navigate(['new'], {relativeTo: this.activatedRoute})
   }
 
-  toProfile(){
+  toProfile() {
     this.router.navigate(['profile'], {relativeTo: this.activatedRoute})
   }
 
-  setImg(){
-    if (this.user.imageUrl){
-      this.imgUrl = this.user.imageUrl
+  setImg() {
+    if (!this.authService.loggedUser.imageUrl) {
+      return 'assets/CuUserLogo.png'
     } else {
-      this.imgUrl = 'assets/CuUserLogo.png'
+      return this.authService.loggedUser.imageUrl
     }
   }
 
